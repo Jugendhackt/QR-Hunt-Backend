dc25620103794152b9b3e95cdf2d0340b3047fa4
fingadumbledore <fingadumbledore@gmail.com>
2022-06-18 12:40:15 +0200

    new file:   db/Cargo.toml 	new file:   db/src/main.rs 	new file:   web_server/Cargo.toml 	new file:   web_server/src/main.rs

---
+++ db/src/main.rs
@@ -0,0 +1,25 @@
+use rusqlite::{Connection, Result};
+use rusqlite::NO_PARAMS;
+    
+
+fn main() -> Result<()> {
+    let conn = Connection::open("qr.db")?;
+
+    conn.execute(
+        "create table if not exists qr_code (
+             id integer primary key,
+             content text not null unique,
+             qrhash text not null unique
+         )",
+        NO_PARAMS,
+    )?;
+    conn.execute(
+        "create table if not exists user (
+             uuid integer primary key,
+             qrcode_id integer not null references qr_code(id)
+         )",
+        NO_PARAMS,
+    )?;
+
+    Ok(())
+}
