c323c1ff0a783ed6a3a40d380870b3aeb2db2761
fingadumbledore <fingadumbledore@gmail.com>
2022-06-18 17:15:21 +0200

    modified:   db/src/main.rs

--- db/src/main.rs
+++ db/src/main.rs
@@ -8,17 +8,26 @@ fn main() -> Result<()> {
     conn.execute(
         "create table if not exists qr_code (
              id integer primary key,
-             content text not null unique,
-             qrhash text not null unique,
-             kordinaten text not null unique
+             userid integer primary key,
+             hash text not null unique,
+
          )",
         NO_PARAMS,
     )?;
     conn.execute(
         "create table if not exists user (
-             uuid integer primary key,
-             qrcode_id integer not null references qr_code(id)
+             id integer primary key,
+             namen text not null unique,
              pwhash text not null unique,
+         )",
+        NO_PARAMS,
+    )?;
+    conn.execute(
+        "create table if not exists qr_lookup (
+             id integer primary key,
+             content text not null unique,
+             hash text not null unique,
+             kordinaten text not null unique
          )",
         NO_PARAMS,
     )?;
