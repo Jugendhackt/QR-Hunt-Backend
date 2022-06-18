3b727bd099e4c54f74c38d97ed67b49109acad8c
fingadumbledore <fingadumbledore@gmail.com>
2022-06-18 15:35:10 +0200

    modified:   api/src/main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,16 +1,27 @@
 mod structs;
 
 use actix_web::{get, post, web, App, HttpServer, Responder};
+use actix_cors::Cors;
 use structs::QrCode;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_header()
+            .allow_any_method()
+            .allow_any_origin();
+           //.allowed_origin("10.20.4.14")
+           //.allowed_methods(vec!["GET", "POST"])
+           //.allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+           //.allowed_header(header::CONTENT_TYPE);
         App::new()
             .service(index)
             .service(hash)
+            .wrap(cors)
     })
-    .bind(("127.0.0.1", 8080))?
+        .workers(4)
+        .bind(("10.20.6.182", 8080))?
     .run()
     .await
 }
@@ -23,6 +34,8 @@ async fn index() -> impl Responder {
 
 #[post("/")]
 async fn hash(qr : web::Json<QrCode>) -> impl Responder {
-    println!("{}\n", qr.id);
+    println!("{}, {}\n", qr.id, qr.content);
+
     qr
+    // access-conrol-allow-origin: 10.20.4.14
 }
